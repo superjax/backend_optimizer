d1aab0528415ecc82653acb02f9b04000de7a188
James Jackson <superjax08@gmail.com>
2017-04-22 12:52:14 -0600

    simple add example working

--- python/optimizer.py
+++ python/optimizer.py
@@ -1,11 +1,3 @@
-from backend_optimizer._backend_optimizer_wrapper_cpp import BackendOptimizer
+from backend_optimizer import example
 
-optimizer = BackendOptimizer()
-
-nodes = [['0_000', 0, 1, 2], ['0_001', 3, 4, 5]]
-edges = [1.0, 1.0]
-fixed_node = 1
-
-optimizer.new_graph(nodes, edges, fixed_node)
-
-print(optimizer.get_optimized())
+debug = 1
--- src/backend_optimizer/__init__.py
+++
@@ -1,4 +0,0 @@
-from backend_optimizer._backend_optimizer_wrapper_cpp import BackendOptimizer
-
-def __init__():
-    Optimizer = BackendOptimizer()
