7fada64e24fe98a1ab4dd084714626739a7a2309
Brendon Forsgren <bforsgren29@gmail.com>
2018-08-10 09:06:01 -0600

    Got rid of everything using the backend.py file. Now getting ready to add stuff to get the plots for the paper.

--- python/main.py
+++ python/main.py
@@ -1,4 +1,3 @@
-from backend import *
 from keyframe_matcher_sim import KeyframeMatcher
 from robot import *
 from controller import *
@@ -9,29 +8,23 @@ import time
 
 if __name__ == "__main__":
     stamp = time.time()
-    os.chdir("plots")
-    os.mkdir(str(stamp))
-    os.chdir(str(stamp))
+    if not os.path.isdir("tests/well_conditioned/demo_2_plots"):
+        os.mkdir("tests/well_conditioned/demo_2_plots")
+    os.chdir("tests/well_conditioned")
     dt = 0.1    
     time = np.arange(0, 300.01, dt)
 
     robots = []
     controllers = []
-    num_robots = 2
+    num_robots = 2  # Will up to 10 later. Start with 2 while familiarizing myself.
     KF_frequency_s = 0.5
     plot_frequency_s = 10
 
-    start_pose_range = [5, 3, 2]
+    start_pose_range = [5, 3, 2]  # TODO Change this when I have more robots
 
     start_poses = [[randint(-start_pose_range[0], start_pose_range[0])*10,
                     (-1)**r * start_pose_range[1]*10,
                     -np.pi/2.0 if r % 2 == 0 else np.pi/2.0] for r in range(num_robots)]
-    # start_poses[0] = [0, 0, 0]
-    # start_poses[1] = [10, 0, pi/2]
-    # start_poses[2] = [10, 10, pi]
-    # start_poses[3] = [0, 10, 3*pi/2]
-
-    # start_poses[1] = [10, 0, pi/2]
 
     P_perfect = np.array([[0.00001, 0, 0], [0, 0.00001, 0], [0, 0, 0.00001]])
     G = np.array([[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]])
@@ -43,11 +36,6 @@ if __name__ == "__main__":
     controllers = [Controller(start_poses[r]) for r in range(num_robots)]
     robots = [Robot(r, G, start_poses[r]) for r in range(num_robots)]
 
-    backend = Backend()
-
-    for r in robots:
-        backend.add_agent(r)
-
     for t in tqdm(time):
         for r in range(num_robots):
             # Run each robot through the trajectory
@@ -57,11 +45,6 @@ if __name__ == "__main__":
                 # Declare a new keyframe
                 edge, KF = robots[r].reset()
 
-                # Add odometry to all maps
-                backend.add_odometry(r, str(r) + "_" + str(robots[r].keyframe_id() - 1).zfill(3),
-                         str(r) + "_" + str(robots[r].keyframe_id()).zfill(3), G,
-                         edge, KF)
-
                 # Add the keyframe to the kf_matcher
                 kf_matcher.add_keyframe(KF, str(r) + "_" + str(robots[r].keyframe_id()).zfill(3))
 
@@ -69,20 +52,3 @@ if __name__ == "__main__":
         if t % plot_frequency_s == 0 and t > 0:
             # look for loop closures
             loop_closures = kf_matcher.find_loop_closures()
-            for lc in loop_closures:
-                backend.add_loop_closure(**lc)
-            backend.plot()
-
-    backend.finish_up()
-    # backend.plot()
-
-    print('Making movie - this make take a while')
-    os.chdir('movie')
-    os.system("mencoder mf://optimized*.png -mf w=800:h=600:fps=10:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o optimized.avi")
-    os.system("mencoder mf://truth*.png -mf w=800:h=600:fps=10:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o truth.avi")
-
-    plt.show()
-
-
-
-    debug = 1
